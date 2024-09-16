fa3b980156e735dda586033c59190c27eb300c62
Ghaniyat27 <ganiyat4real1@gmail.com>
2024-09-16 22:07:27 +0000

    0x05-Node_JS_basic

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Ghaniyat Babalola <https://github.com/Ghaniyat27>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
